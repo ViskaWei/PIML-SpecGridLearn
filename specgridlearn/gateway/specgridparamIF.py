c05494a4e9432984b71c060df349de63083c55d1
ViskaWei <swei20@jhu.edu>
2022-04-10 13:44:41 -0400

    add  paramIF

--- specgridlearn/gateway/specgridparamIF.py
+++ specgridlearn/gateway/specgridparamIF.py
@@ -1,5 +1,8 @@
+from base.interface.gateway.baseparamIF import MultiParamIF
+from spec.interface.gateway.specparamIF import SpecParamIF
+from grid.interface.gateway.gridparamIF import GridParamIF
 
-
-class SpecGridParamIF():
-    def set(self, PARAM):
-        return PARAM
+class SpecGridParamIF(MultiParamIF):
+    def __init__(self) -> None:
+        super().__init__()
+        self.paramIF_list = [SpecParamIF(), GridParamIF()]

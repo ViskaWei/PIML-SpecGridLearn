86c3d226b897316e879f4331db61da8bd49f98dc
ViskaWei <swei20@jhu.edu>
2022-04-10 23:14:49 -0400

    using unit_coord

--- specgridlearn/gateway/specgridparamIF.py
+++ specgridlearn/gateway/specgridparamIF.py
@@ -1,8 +1,8 @@
 from base.interface.gateway.baseparamIF import MultiParamIF
-from spec.interface.gateway.specparamIF import SpecParamIF
-from grid.interface.gateway.gridparamIF import GridParamIF
+from spec.interface.gateway.specparamIF import StellarSpecParamIF
+from grid.interface.gateway.gridparamIF import StellarGridParamIF
 
 class SpecGridParamIF(MultiParamIF):
     def __init__(self) -> None:
         super().__init__()
-        self.paramIF_list = [SpecParamIF(), GridParamIF()]
+        self.paramIF_list = [StellarSpecParamIF(), StellarGridParamIF()]
